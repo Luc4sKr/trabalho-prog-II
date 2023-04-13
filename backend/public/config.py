9125a3468e2fcb54f2be9e5719b13d62e2dc355c
Lucas <lucaskreuch77@gmail.com>
2023-04-13 11:16:01 -0300

    first commit

---
+++ backend/public/config.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+path = os.path.dirname(os.path.abspath(__file__))
+arquivobd = os.path.join(path, 'saude.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+arquivobd
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
