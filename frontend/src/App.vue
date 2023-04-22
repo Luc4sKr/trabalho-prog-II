9f0d3e564c0377ca98915d200c5a6186b48fd05a
Lucas <lucaskreuch77@gmail.com>
2023-04-22 20:10:20 -0300

    new project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,93 +1,26 @@
 <template>
-    <HeaderComponent />
-    <HomeComponent />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HeaderComponent from "./components/HeaderComponent.vue";
-import HomeComponent from "./components/HomeComponent.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-    name: 'App',
-    components: {
-        HeaderComponent,
-        HomeComponent
-    },
-    data() {
-        return {
-            minion: {
-                id: 1,
-                name: "Minion 1",
-
-                hair: {
-                    id: 1,
-                    hair_name: "Default"
-                },
-                width: {
-                    id: 1,
-                    minion_width: "default",
-                    css_width_reference: "--default-width",
-                    css_dungarees_reference: "--minion--dungarees-default" 
-                },
-                height: {
-                    id: 1,
-                    minion_height: "default",
-                    css_height_reference: "--default-height"
-                },
-                pose: {
-
-                }
-            }
-        }
-    }
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-
-:root {
-    /* COLORS */
-    --yellow: #fce55b;
-    --blue: #336188;
-
-    /* --- MINION SIZES --- */
-
-    /* SMALL */
-    --small-width: 90px;
-    --small-height: 170px;
-
-    /* DEFAULT */
-    --default-width: 100px;
-    --default-height: 180px;
-
-    /* HIGH */
-    --high-width: 120px;
-    --high-height: 190px;
-
-    /* DUNGAREES */
-    --minion-dungarees-small: -5px;
-    --minion-dungarees-default: -7px;
-    --minion-dungarees-high: -11px;
-
-    /* POSE */
-    --t-pose: 90deg;
-    --default-pose: 30deg;
-
-    /* --- MINION --- */
-    --minion-width: var(--default-width);
-    --minion-height: var(--default-height);
-    --minion-dungarees: var(--minion-dungarees-default);
-    --minion-pose: var(--default-pose);
-}
-
-* {
-    margin: 0px;
-    padding: 0px;
-}
-
-body {
-    font-family: 'Roboto', sans-serif;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- frontend/src/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-function getPropertyStyleVal(val) {
-    return getComputedStyle(document.documentElement).getPropertyValue(val);
-}
-
-export function setPropertyStyleVal(property, reference) {
-    let ref = getPropertyStyleVal(reference);
-    document.body.style.setProperty(property, ref);
-}
-
