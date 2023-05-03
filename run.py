46816e4d34e3308181c3398ba23c1e1904853c02
Lucas <lucaskreuch77@gmail.com>
2023-05-03 15:09:04 -0300

    run script

---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+from sys import platform
+
+if __name__ == "__main__":
+
+    backend_path = ".\\backend\\server.py"
+    frontend_path = ".\\frontend\\dist"
+
+    match platform:
+        case ("win32"):
+            os.system(f"start cmd /k python {backend_path}")
+            os.system(f"start cmd /k python -m http.server -d {frontend_path}")
+        
+        case ("linux"):
+            pass
